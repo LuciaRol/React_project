fa47c2e492373bfd69978330ae9cdf4419730f2c
LuciaRol <luciarol87@gmail.com>
2024-02-02 11:04:51 +0100

    primer componente

---
+++ src/Comp.jsx
@@ -0,0 +1,12 @@
+function Comp(){
+    let titulo = "Cargando contenido de titulo"
+    return(
+    <>
+        <h1>Primer componente</h1>
+        <h2>{titulo}</h2>
+    </>
+    );
+
+}
+
+export default Comp
