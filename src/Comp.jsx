afdb338081349b7f9c6b27d0dc9117129081f48f
LuciaRol <luciarol87@gmail.com>
2024-02-02 12:27:48 +0100

    boton salta evento

--- src/Comp.jsx
+++ src/Comp.jsx
@@ -1,4 +1,8 @@
 function Comp(){
+    function trataClick(){
+        console.log("Has clicado en el boton")
+    }
+
     let titulo = "Cargando contenido de titulo";
     let nombres=['lu', 'paco', 'sam', 'pablo'];
     let lista = nombres.map(nombre =>
@@ -11,6 +15,7 @@ function Comp(){
         <h1>Primer componente</h1>
         <h2>{titulo}</h2>
 
+        <button onClick={trataClick}>Hazme click</button>
         <ul>
             {lista}
         </ul>
