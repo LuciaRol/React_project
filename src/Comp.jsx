b8a61b6f47c57f246924c0ee2759ef1a6ea5aca4
luciarol <luciarol87@gmail.com>
2024-02-05 21:25:47 +0100

    calculadora

--- src/Comp.jsx
+++ src/Comp.jsx
@@ -1,38 +1,107 @@
 import { useState } from 'react'
 
-function Comp({ param, param2=0 }){
+function Calculadora() {
+    const [display, setDisplay] = useState('0');
+    const [numeroActual, setNumeroActual] = useState('');
+    const [numeroAnterior, setNumeroAnterior] = useState('');
+    const [operador, setOperador] = useState('');
+  
+    const manejarClickNumero = (num) => {
+      if (display === '0') {
+        setDisplay(num.toString());
+      } else {
+        setDisplay(display + num.toString());
+      }
+      setNumeroActual(numeroActual + num.toString());
+    };
+  
+    const manejarClickOperador = (op) => {
+      if (numeroActual !== '') {
+        if (numeroAnterior !== '') {
+          manejarIgual();
+        }
+        setNumeroAnterior(numeroActual);
+        setNumeroActual('');
+        setOperador(op);
+        setDisplay(''); // Aquí se borra el número de la pantalla
+      }
+    };
+  
+    const manejarIgual = () => {
+      let resultado;
+      switch (operador) {
+        case '+':
+          resultado = parseFloat(numeroAnterior) + parseFloat(numeroActual);
+          break;
+        case '-':
+          resultado = parseFloat(numeroAnterior) - parseFloat(numeroActual);
+          break;
+        case '*':
+          resultado = parseFloat(numeroAnterior) * parseFloat(numeroActual);
+          break;
+        case '/':
+          resultado = parseFloat(numeroAnterior) / parseFloat(numeroActual);
+          break;
+        default:
+          return;
+      }
+      setDisplay(resultado.toString());
+      setNumeroAnterior(resultado.toString());
+      setNumeroActual('');
+      setOperador('');
+    };
+  
+    const manejarLimpiar = () => {
+      setDisplay('0');
+      setNumeroActual('');
+      setNumeroAnterior('');
+      setOperador('');
+    };
 
-    const [titulo, setTitulo] = useState('Cargando contenido de titulo');
 
+    
+    const generarBotonesNumericos = () => {
+    const botones = [];
+    for (let i = 0; i <= 9; i++) {
+      botones.push(
+        <button key={i} onClick={() => manejarClickNumero(i)}>{i}</button>
+      );
+    }
+    return botones;
+  };
+
+    /*
+    
     function trataClick(e){
         // como en js plano, esta es la forma de que solo salga en pantalla en item clicado
         setTitulo ("Has clicado en " + e.target.innerHTML)
         console.log(titulo);
     }
 
-
+    */
 
     /* let titulo = "Cargando contenido de titulo"; */
-    let nombres=['lu', 'nerdo', 'sam', 'pablo'];
+    /*let nombres=['lu', 'nerdo', 'sam', 'pablo'];
     let lista = nombres.map(nombre =>
             <li key={nombre}>{nombre}</li>
         )
+    */
 
-  
-    return(
-    <>
-        <h1>Primer componente</h1>
-        <h2>Recibe como param = {param}</h2>
-        <h2>Recibe como param2 = {param2}</h2>
-
-        <button onClick={trataClick}>Hazme click</button>
-        <ul onClick={trataClick}>
-            {lista}
-        </ul>
-     
-        </>
-    );
+    return (
+        <div className="calculadora">
+            <div className="display">{display}</div>
+            <div className="botones">
+            {generarBotonesNumericos()}
+            <button onClick={() => manejarIgual()}>=</button>
+            <button onClick={() => manejarClickOperador('+')}>+</button>
+            <button onClick={() => manejarClickOperador('-')}>-</button>
+            <button onClick={() => manejarClickOperador('*')}>*</button>
+            <button onClick={() => manejarClickOperador('/')}>/</button>
+            <button onClick={() => manejarLimpiar()}>C</button>
+            </div>
+        </div>
+        );
 
 }
 
-export default Comp
+export default Calculadora
